35506dbf9cd4f9a959791b99ee4c3bcbbbb47159
RanitBehera <ranitbehera1998@gmail.com>
2023-11-14 15:56:02 +0530

    random commit

---
+++ examples/ex_data_extractor.py
@@ -0,0 +1,18 @@
+import sys
+import os
+sys.path.append(os.getcwd())
+
+import numpy as np
+import modules as mp
+
+op=mp.BaseDirectory(r"D:\Ubuntu Back\Desktop\Sim\hydro\std_hydro\seed_181170\fg11\output")
+# op=mp.BaseDirectory(r"D:\Ubuntu Back\Desktop\Sim_640")
+
+
+pos=op.PART(17).DarkMatter.Velocity.ReadValues()
+
+at=0
+while(at>=0):
+    at=input("index : ")
+    at=int(at)
+    print(pos[at])
---
+++ examples/ex_read_part17.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+import sys
+import os
+sys.path.append(os.getcwd())
+import modules as mp
+
+
+op=mp.BaseDirectory(r"D:\Ubuntu Back\Desktop\Sim\hydro\std_hydro\seed_181170\fg11\output")
+pos=op.PART(17).BlackHole.Position.ReadValues()
+vel=op.PART(17).BlackHole.Velocity.ReadValues()
+id=op.PART(17).BlackHole.ID.ReadValues()
+mass=op.PART(17).BlackHole.Mass.ReadValues()
+
+di=int(input("Debug Index :"))
+while(di>-1):
+    print("ID: ",id[di])
+    print("mass: ",mass[di])
+    print("Pos: ",pos[di])
+    print("Vel: ",vel[di])
+    di=int(input("Debug Index :"))
