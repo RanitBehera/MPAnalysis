bfc8bd39429a9bf6b0d39eb58cd16140dd203db5
RanitBehera <ranitbehera1998@gmail.com>
2023-12-06 07:10:58 +0530

    colossus added

--- scripts/rockstar/plt_halo_mass_function.py
+++ scripts/rockstar/plt_halo_mass_function.py
@@ -14,7 +14,7 @@ SNAP_NUMBER             = 16
 REDSHIFT                = 12
 ROCKSTAR_HALO_FILENAME  = "halos_PART_"+'{:03}'.format(SNAP_NUMBER)+".hdf5.0.ascii"
 INCLUDE_PIG             = True
-BINDEXSTEP              = 0.2
+BINDEXSTEP              = 0.25
 SHOW_DEVIATION_PLOT     = False
 
 
@@ -30,7 +30,8 @@ HUBBLE_H        = cfg.h0
 
 # --- SIMULATION HALO MASS FUNCTION
 def SimHMF(Mass,LogBinStep):
-    log10_Mass=numpy.log10(Mass)
+    # log10_Mass=numpy.log10(Mass)
+    log10_Mass=numpy.log(Mass)
 
     log10_bin_start=numpy.floor(min(log10_Mass))
     log10_bin_end=numpy.ceil(max(log10_Mass))
@@ -49,12 +50,25 @@ def SimHMF(Mass,LogBinStep):
 
 
 # --- PLOTTING METHODS
+
 def Plot_Model_HMF(axis_handle,model_name,**kwargs):
     mf = MassFunction(z = REDSHIFT,hmf_model=model_name,cosmo_params={"Om0":OMEGA_M,"H0":HUBBLE_H*100,"Tcmb0":2.7255})
     mf.Mmin=7
     mf.Mmax=12
     h=HUBBLE_H
     axis_handle.plot(mf.m,mf.dndlog10m*(h**3),**kwargs)
+
+my_cosmo = {'flat': True, 'H0': 69.7, 'Om0': 0.2814, 'Ob0':0.0464, 'sigma8': 0.81, 'ns': 0.971}
+from colossus.cosmology import cosmology
+cosmology.setCosmology("my_cosmo",my_cosmo)
+from colossus.lss import mass_function
+
+def Plot_Colo_HMF(axis_handle,model_name,mdef="fof",**kwargs):
+    z = REDSHIFT
+    M = 10**numpy.arange(7.0, 12, 0.1)
+    mfunc = mass_function.massFunction(M, z, mdef = mdef, model = model_name, q_out = 'dndlnM')
+    axis_handle.plot(M,mfunc*(HUBBLE_H**3),**kwargs)
+    
     
 def Plot_Model_Deviation(axis_handle,model_name,dn_dlogM,error,**kwargs):
     # ----------- Make model hmf automatic
@@ -83,7 +97,8 @@ def Plot_PIG_HMF(axis_handle,GADGET_PATH=GADGET_PATH,SNAP_NUMBER=SNAP_NUMBER,BIN
     log10_M,dn_dlogM,error=SimHMF(cdm_mass,BINDEXSTEP)
     fi,fe=0,-1
     error=error/2
-    axis_handle.errorbar(10**log10_M[fi:fe],dn_dlogM[fi:fe],error[fi:fe],**kwargs)
+    # axis_handle.errorbar(10**log10_M[fi:fe],dn_dlogM[fi:fe],error[fi:fe],**kwargs)
+    axis_handle.errorbar(numpy.e**log10_M[fi:fe],dn_dlogM[fi:fe],error[fi:fe],**kwargs)
     
     # ------ Get axis handle 2 and kwargs
     # Plot_Model_Deviation(ac,"ST",dn_dlogM,error,alpha=0.2,edgecolor=(0,0,0,0),color="m")
@@ -91,14 +106,16 @@ def Plot_PIG_HMF(axis_handle,GADGET_PATH=GADGET_PATH,SNAP_NUMBER=SNAP_NUMBER,BIN
 
 def Plot_RKS_HMF(axis_handle,HALOFILE_PATH=HFILEPATH,BINDEXSTEP=BINDEXSTEP,**kwargs):
     data=numpy.loadtxt(HALOFILE_PATH)
-    m_vir=data[:,mp.ascii.mvir]
+    # m_vir=data[:,mp.ascii.mvir]
+    m_vir=data[:,mp.ascii.m200c]
     mask=~(m_vir==0)
     m_vir=m_vir[mask]
 
     log10_M,dn_dlogM,error=SimHMF(m_vir,BINDEXSTEP)
     fi,fe=0,-1
     error=error/2
-    axis_handle.errorbar(10**log10_M[fi:fe],dn_dlogM[fi:fe],error[fi:fe],**kwargs)
+    # axis_handle.errorbar(10**log10_M[fi:fe],dn_dlogM[fi:fe],error[fi:fe],**kwargs)
+    axis_handle.errorbar(numpy.e**log10_M[fi:fe],dn_dlogM[fi:fe],error[fi:fe],**kwargs)
 
 def Plot_RKS_ONLY_HMF(axis_handle,PATH=HFILEPATH,BINDEXSTEP=BINDEXSTEP,**kwargs):
     Plot_RKS_HMF(axis_handle,HALOFILE_PATH=PATH,BINDEXSTEP=BINDEXSTEP,**kwargs)
@@ -152,13 +169,20 @@ else:
 
 # Possible model HMF
 # PS, ST, Behroozi, Angulo, Bhattacharya, Courtin, Crocce, Ishiyama, Jenkins, Manera, Peacock, Pillepich, Reed03, Reed07, Warren, Watson, Tinker08, Tinker10
-Plot_Model_HMF(ac,"PS",label="Press-Schechter",color='b')
-Plot_Model_HMF(ac,"ST",label="Seith-Tormen",color='k')
-Plot_Model_HMF(ac,"Behroozi",label="Behroozi",color='m')
+# Plot_Model_HMF(ac,"PS",label="Press-Schechter",color='b')
+# Plot_Model_HMF(ac,"ST",label="Seith-Tormen (hmf)",color='k')
+# Plot_Model_HMF(ac,"Behroozi",label="Behroozi",color='m')
+
+Plot_Colo_HMF(ac,'sheth99',ls="--",label="Sheth99 (colossus, fof)",color='r',lw=1)
+Plot_Colo_HMF(ac,'press74',ls="--",label="Press74 (colossus, fof)",color='m',lw=1)
+# Plot_Colo_HMF(ac,'comparat17',ls="-",label="Comparat17 (colossus, vir)",mdef='vir',color='b',lw=1)
+Plot_Colo_HMF(ac,'bocquet16',ls="-",label="Bocquet (colossus, 200c)",mdef='200c',color='b',lw=1)
 
 if INCLUDE_PIG:
-    Plot_PIG_HMF(ac,fmt='.--',capsize=2,color='r',label="FoF (MP-Gadget)",lw=1)
-    
+    Plot_PIG_HMF(ac,fmt='--',capsize=1,color='r',label="FoF (MP-Gadget)",lw=1)
+Plot_RKS_ONLY_HMF(ac,fmt='-',capsize=1,color='g',label="Rockstar",lw=1)
+Plot_RKS_GAL_HMF(ac,fmt='-',capsize=1,color='b',label="Rockstar Galaxies",lw=1)
+
 
 # Plot_RKS_ONLY_HMF(ac,"/mnt/home/student/cranit/Work/RKSG_Benchmark/L50N640c/LL_Prof/RKS/L18" +os.sep+"halos_PART_036.hdf5.0.ascii",fmt='.-',capsize=2,label="Rockstar (0.18)")
 # Plot_RKS_ONLY_HMF(ac,"/mnt/home/student/cranit/Work/RKSG_Benchmark/L50N640c/LL_Prof/RKS/L20" +os.sep+"halos_PART_036.hdf5.0.ascii",fmt='.-',capsize=2,label="Rockstar (0.20)")
@@ -177,8 +201,7 @@ if INCLUDE_PIG:
 # Plot_RKS_ONLY_HMF(ac,"/mnt/home/student/cranit/Work/RKSG_Benchmark/L50N640c/LL_Prof/RKSG/L30" +os.sep+"halos_PART_036.hdf5.0.ascii",fmt='.-',capsize=2,label="Rockstar Galaxies (0.30)")
 
 
-Plot_RKS_ONLY_HMF(ac,fmt='.-',capsize=2,color='g',label="Rockstar")
-Plot_RKS_GAL_HMF(ac,fmt='.-',capsize=2,color='b',label="Rockstar Galaxies")
+
 
 
 
@@ -191,7 +214,8 @@ ac.set_yscale('log')
 ac.grid(alpha=0.5)
 ac.legend()
 # ac.set_xticks([])
-ac.set_ylabel("$dn/dlog_{10}(M/M_\odot)$",fontsize=12)
+# ac.set_ylabel("$dn/dlog_{10}(M/M_\odot)$",fontsize=12)
+ac.set_ylabel("$dn/dln(M/M_\odot)$",fontsize=12)
 
 if SHOW_DEVIATION_PLOT:
     ae.set_xscale('log')
@@ -217,6 +241,6 @@ if not SHOW_DEVIATION_PLOT:
 ac.set_title("Dark Matter Halo Mass Function Comparision (z="+str(numpy.round(REDSHIFT,2))+") : L = $" + str(cfg.BOX_SIZE) + "$ Mpc , N = $640^3$")
 # plt.tight_layout()
 # plt.show()
-plt.savefig("/mnt/home/student/cranit/Work/RKSG_Benchmark/results/ZD_Prof/snap" + '{:03}'.format(SNAP_NUMBER) + ".png",dpi=200)
-plt.savefig("/mnt/home/student/cranit/Work/RKSG_Benchmark/results/ZD_Prof/snap" + '{:03}'.format(SNAP_NUMBER) + ".svg")
+plt.savefig("/mnt/home/student/cranit/Work/RKSG_Benchmark/results/ZD_Prof_cols/colossus_m200c/snap" + '{:03}'.format(SNAP_NUMBER) + ".png",dpi=200)
+plt.savefig("/mnt/home/student/cranit/Work/RKSG_Benchmark/results/ZD_Prof_cols/colossus_m200c/snap" + '{:03}'.format(SNAP_NUMBER) + ".svg")
 
